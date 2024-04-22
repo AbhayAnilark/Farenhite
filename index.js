42588a383b72f5a079419d6765f9aa930d913d7e
AbhayAnilark <abhayanilark@gmail.com>
2024-04-22 13:03:16 +0530

    new css

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+
 document.getElementById("b1").onclick=function(){
     let x=document.getElementById("sel1").value;
     x=Number(x);
     let y= ((x-32)*5)/9;
-    document.getElementById("d").innerHTML= "Temperatur in Celcius is " + y;
+    y=y.toFixed(2);
+
+    document.getElementById("d").innerHTML= "INPUT : " + x + "<br>";
+    document.getElementById("d").innerHTML+= "Temperatur in Celcius is " + y;
 
 }
 document.getElementById("b2").onclick=function(){
     let x=document.getElementById("sel1").value;
     x=Number(x);
-    let y=  9*x/5 + 32; 
-    document.getElementById("d").innerHTML= "Temperatur in Celcius is " + y;
+    let y= 9*x/5 + 32; 
+    y=y.toFixed(2);
+
+    document.getElementById("d").innerHTML= "INPUT : " + x + "<br>";
+    document.getElementById("d").innerHTML+= "Temperatur in Celcius is " + y;
 
 }
