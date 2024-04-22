e630d685e8f65706b32901a93f104d4c8f2dccce
AbhayAnilark <abhayanilark@gmail.com>
2024-04-22 11:54:10 +0530

    new feature

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ document.getElementById("b1").onclick=function(){
     document.getElementById("d").innerHTML= "Temperatur in Celcius is " + y;
 
 }
+document.getElementById("b2").onclick=function(){
+    let x=document.getElementById("sel1").value;
+    x=Number(x);
+    let y=  9*x/5 + 32; 
+    document.getElementById("d").innerHTML= "Temperatur in Celcius is " + y;
+
+}
